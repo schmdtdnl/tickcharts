cb6a231e7af83f7b494438fde3369e44ee4f4048
Schmidt Daniel <d.schmidt@mito.hu>
2013-10-25 16:53:04 +0200

    excel report data prepare(not working now, but i have to go home now:))

--- TickExcelReport.php
+++ TickExcelReport.php
@@ -5,10 +5,11 @@ class TickExcelReport
 	private $report_data;
 	private $project_id;
 	private $phpexcel;
+	private $report;
 	
-	public function __construct($project_id)
+	public function __construct(TickReport $report)
 	{
-		$this->project_id = $project_id;
+		$this->report = $report;
 		
 		require_once ('Classes/PHPExcel/IOFactory.php');
 		$this->phpexcel = PHPExcel_IOFactory::createReader('Excel2007');
@@ -16,16 +17,78 @@ class TickExcelReport
 	
 	private function set_document_properties()
 	{
-		
+		//set base information title, author, etc.
 	}
 	
-	public function get_report()
+	public function get_report($project_id)
+	{
+		$this->project_id = $project_id;
+		$this->get_tasks();
+	}
+	
+	private function prepare_report_data()
 	{
 		
 	}
 	
-	private function prepare_report_data($project_id)
+	/*
+	 * Get tasks, names(percents per task), time
+	 */
+	private function get_tasks()
 	{
+		require_once('project_configs/982715_config.php');
+		$project = $this->report->get_project(982715);
+		try 
+		{ 
+			$project_xml = new SimpleXMLElement($project);	
+		} 
+		catch (Exception $e) 
+		{
+			echo 'bad xml';
+			return false;
+		}
+		// it's not working :)
+		foreach ($project_xml->project->tasks as $one)
+		{
+			if (array_key_exists($one['id'],$project_config['tasks']))
+			{
+				$this->report_data[$one['id']] = array
+				(
+					'id' 			=> $one['id'],
+					'name' 			=> $one['name'],
+					'budget'		=> $one['budget'],
+					'hours'			=> $one['sum_hours'],
+					'plus_hours' 	=> ($one['sum_hours'] > $one['budget']) ? ($one['sum_hours'] - $one['budget']) : (0),
+					'fee'			=> $project_config['tasks'][$one['id']]['price'],
+					'price_wo_decrease' => $this->calculate_price($one['sum_hours'], $project_config['tasks'][$one['id']]['price'], $one['budget'], false),
+					'price_w_decrease'	=> $this->calculate_price($one['sum_hours'], $project_config['tasks'][$one['id']]['price'], $one['budget'], true),
+				);
+			}
+		}
+	}
 	
+	/**
+	 * This function has do be developed for each project separately(by the project contract)
+	 */
+	private function calculate_price($hours, $fee, $budget, $decrease = false)
+	{
+		if (!$decrease)
+		{
+			return ($hours * $fee);
+		}
+		
+		$plus_hours = ($hours > $budget) ? ($hours - $budget) : (0);
+		
+		if ($plus_hours == 0)
+		{
+			return $hours * $fee;
+		}
+		
+		// 5% from the plus time is free
+		$decreased_plus_time = $plus_hours * 0.95;
+		
+		$all_hours = $budget + $decreased_plus_time;
+		
+		return $all_hours * $fee;
 	}
 }
--- source.php
+++ source.php
@@ -1,16 +1,21 @@
 <?php
 require_once('TickReport.php');
 require_once('TickChart.php');
+require_once('TickExcelReport.php');
 require_once('config.php');
 
 $report = new TickReport($config['company'], $config['email'], $config['password']);
 $charts = new TickChartMonthlyTime($report);
+$xls = new TickExcelReport($report);
 
 switch ($_GET['f'])
 {
 	case 'get_project':
 		$data = $charts->get_chart_data($_GET['id']);
 		echo json_encode($data);
+		break;
+	case 'get_excel_report':
+		$xls->get_report($_GET['id']);
 		break;
 	case 'get_projects':
 	default:
