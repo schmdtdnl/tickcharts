00e01d1fa783a6aa8940b4ef262b6b24ea6aa6c3
Daniel Schmidt <hello@danielschmidt.me>
2013-11-06 07:43:37 +0100

    new project config

---
+++ project_configs/992661_config.php
@@ -0,0 +1,104 @@
+<?php
+
+$project_config = array
+(
+	'meta' => array
+	(
+		'name' => 'November',
+	),
+	'time' => array
+	(
+		'year'	=> '2013',
+		'month'	=> '11',
+		'start' => '2013-11-01',
+		'end'	=> '2013-11-30',
+	),
+	'tasks' => array
+	(
+		'5614762' => array
+		(
+			'id' => 5614762,
+			'name' => 'TechPM',
+			'price'=> 11250,
+		),
+		'5614763' => array
+		(
+			'id' => 5614763,
+			'name' => 'Frontend',
+			'price'=> 11700,
+		),
+		'5614764' => array
+		(
+			'id' => 5614764,
+			'name' => 'Junior data',
+			'price'=> 11700,
+		),
+		'5614765' => array
+		(
+			'id' => 5614765,
+			'name' => 'Head of data',
+			'price'=> 20700,
+		),
+		'5614766' => array
+		(
+			'id' => 5614766,
+			'name' => 'Senior data',
+			'price'=> 13500,
+		),
+		'5614767' => array
+		(
+			'id' => 5614767,
+			'name' => 'Backend architect',
+			'price'=> 13950,
+		),
+		'5614768' => array
+		(
+			'id' => 5614768,
+			'name' => 'Backend developer',
+			'price'=> 11250,
+		),
+		'5614769' => array
+		(
+			'id' => 5614769,
+			'name' => 'Tech PM CTO',
+			'price'=> 16650,
+		),
+		'5614770' => array
+		(
+			'id' => 5614770,
+			'name' => 'Graphic designer',
+			'price'=> 13950,
+		),
+		'5614771' => array
+		(
+			'id' => 5614771,
+			'name' => 'Graphic designer',
+			'price'=> 13950,
+		),
+		'5614772' => array
+		(
+			'id' => 5614772,
+			'name' => 'UX designer',
+			'price'=> 14850,
+		),
+		'5614773' => array
+		(
+			'id' => 5614773,
+			'name' => 'SEM',
+			'price'=> 14850,
+		),
+		'5614774' => array
+		(
+			'id' => 5614774,
+			'name' => 'QA',
+			'price'=> 11250,
+		),
+		'5614775' => array
+		(
+			'id' => 5614775,
+			'name' => 'CW',
+			'price'=> 0,
+		),
+	),
+);
+  
